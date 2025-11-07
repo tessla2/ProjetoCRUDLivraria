fc7b1b5653c0e757190cca464f0c90169964b64d
Tainara Santos <167295256+tessla2@users.noreply.github.com>
2025-11-07 11:33:19 +0000

    Update consultartodos.php

--- consultartodos.php
+++ consultartodos.php
@@ -60,10 +60,10 @@ if (isset($_SESSION["utilizador"])) {
     $utilizador = $_SESSION["utilizador"];
 
     //---------------------------------------Conexão a BD----------------------------------------------------------------------------//    
-    $maquina="fdb1031.runhosting.com";
-    $usernamebd="4433440_tess";
-    $passwordbd="fusrodah2011";
-    $bd="4433440_tess";    
+    $maquina="***";
+    $usernamebd="***";
+    $passwordbd="***";
+    $bd="***";
         
     $ligacao = new PDO("mysql:host=$maquina; dbname=$bd", $usernamebd, $passwordbd);    
     $ligacao->query("SET NAMES utf8");         
